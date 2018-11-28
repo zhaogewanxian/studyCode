1d7a75bdbf040410bb37dbcc3e31aba1ea969797
zhaogewanxian <449244549@qq.com>
2018-11-28 17:57:53 +0800

    fix

--- zookeeper/zookeeper-demo/src/main/java/com/wanxian/zookeeper/ConnectionDemo.java
+++ zookeeper/zookeeper-demo/src/main/java/com/wanxian/zookeeper/ConnectionDemo.java
@@ -1,8 +1,7 @@
 package com.wanxian.zookeeper;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -25,6 +24,23 @@ public class ConnectionDemo {
             });
             countDownLatch.await();
             System.out.println(zooKeeper.getState());//CONNECTED
+            //新增节点
+            zooKeeper.create("/wanxian-node", "1".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            Thread.sleep(1000);
+            Stat stat = new Stat();
+            //得到当前节点的值
+            byte[] bytes = zooKeeper.getData("/wanxian-node", null, stat);
+            System.out.println(new String(bytes));
+            //修改节点
+            zooKeeper.setData("/wanxian-node", "2".getBytes(), stat.getVersion());
+
+            byte[] bytes1 = zooKeeper.getData("/wanxian-node", null, stat);
+            System.out.println(new String(bytes1));
+
+            //删除节点
+            zooKeeper.delete("/wanxian-node", stat.getVersion());
+            zooKeeper.close();
+            System.in.read();
         } catch (Exception e) {
             e.printStackTrace();
         }
