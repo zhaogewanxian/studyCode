512c01467e23fcd62721d60965af3f258df301a6
zhaogewanxian <449244549@qq.com>
2019-03-22 10:14:58 +0800

    修正排序

--- deep-in-java/stage-2/src/main/java/com/wanxian/BubbleSort.java
+++ deep-in-java/stage-2/src/main/java/com/wanxian/BubbleSort.java
@@ -1,5 +1,6 @@
 package com.wanxian;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 /**
@@ -15,20 +16,21 @@ public class BubbleSort<T extends Comparable<T>> implements Sort<T> {
         //= return = 0
         //> return 1
         int size = values.length;//数组长度
-        for (int i = 0; i < size; i++) {//第一层循环
-            T temp = values[i]; //临时存储变量 ,产生临时变量
-            for (int j = i + 1; j < size; j++) {//第二层循环
-                if (temp.compareTo(values[j]) == 1) { //如果前一个元素比后一个大:低位>高位
-                    values[i] = values[j];//调换位置
-                    values[j] = temp; //赋值
-                    break;
+        for (int i = 0; i < size - 1; i++) {//第一层循环
+            for (int j = 0; j < size - i - 1; j++) {//第二层循环
+                if (values[j].compareTo(values[j + 1]) == 1) { //如果前一个元素比后一个大:低位>高位
+                    T temp = values[j]; //临时存储变量 ,产生临时变量
+                    values[j] = values[j+1];//调换位置
+                    values[j+1] = temp; //赋值
                 }
             }
+            System.out.printf("第%d轮：%s\n", i + 1, Arrays.toString(values));
         }
     }
 
     public static void main(String[] args) {
         Integer[] values = Sort.of(2, 1, 5, 3, 4);
+        //Integer[] values = Sort.of(5, 4, 3, 2, 1);
         Sort<Integer> sort = new BubbleSort<>(); //java 7 diamond语法
         sort.sort(values);
         Stream.of(values).forEach(System.out::println);
