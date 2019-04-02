5355d0cbdf8bc9fa50319aaa8607a2951b8b9afa
zhaogewanxian <449244549@qq.com>
2019-04-02 18:55:26 +0800

    web

--- spring-boot/spring-boot-web/src/main/java/com/wanxian/WebApplication.java
+++ spring-boot/spring-boot-web/src/main/java/com/wanxian/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan("com.wanxian.servlet")
+@ServletComponentScan(basePackages = "com.wanxian.servlet")
 public class WebApplication {
 
     public static void main(String[] args) {
--- spring-boot/spring-boot-web/src/main/java/com/wanxian/servlet/MyServlet.java
+++ spring-boot/spring-boot-web/src/main/java/com/wanxian/servlet/MyServlet.java
@@ -1,7 +1,5 @@
 package com.wanxian.servlet;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,25 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
-@WebServlet(name = "myServlet", urlPatterns = "/myServlet", initParams = {
-        @WebInitParam(name = "myname", value = "myvalue")
-})
+@WebServlet(
+        name = "myServlet",
+        urlPatterns = "/myservlet",
+        initParams = {
+                @WebInitParam(name = "myname", value = "luweiying")
+        }
+)
 public class MyServlet extends HttpServlet {
 
     private String value;
 
-    @Autowired
+    @Override
     public void init(ServletConfig servletConfig) throws ServletException {
         super.init(servletConfig);
         value = servletConfig.getInitParameter("myname");
     }
 
-    @Autowired
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Writer writer = response.getWriter();
         ServletContext servletContext = getServletContext();
         servletContext.log("doGet .........");
-        writer.write("hello,world");
+        writer.write("hello," + value);
 
     }
 
