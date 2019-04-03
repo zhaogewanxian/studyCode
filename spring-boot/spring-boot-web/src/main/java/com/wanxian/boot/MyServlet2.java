8d121109a4b2bdfce420b68b197cf187ad8142fb
zhaogewanxian <449244549@qq.com>
2019-04-03 18:21:49 +0800

    springboot-servlet

---
+++ spring-boot/spring-boot-web/src/main/java/com/wanxian/boot/MyServlet2.java
@@ -0,0 +1,13 @@
+package com.wanxian.boot;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet2 extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+
+
+    }
+}
