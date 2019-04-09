27ba9cc229a83608db26c806402323d30ee62a37
zhaogewanxian <449244549@qq.com>
2019-04-10 00:01:15 +0800

    spring-application project init

--- spring-cloud/spring-application/src/main/java/com/wanxian/applicaiton/SpringAnnotationDemo.java
+++ spring-cloud/spring-application/src/main/java/com/wanxian/applicaiton/SpringAnnotationDemo.java
@@ -2,11 +2,22 @@ package com.wanxian.applicaiton;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+/**
+ * @Component 的"派生性" (注解没有extends)
+ * @see Component
+ */
 @Configuration
 public class SpringAnnotationDemo {
 
     public static void main(String[] args) {
+        /**
+         *
+         * Annotation 驱动
+         * 找BeanDefinition
+         * @Bean @Configuration
+         */
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(SpringAnnotationDemo.class);//注册
         applicationContext.refresh(); //启动上下文
