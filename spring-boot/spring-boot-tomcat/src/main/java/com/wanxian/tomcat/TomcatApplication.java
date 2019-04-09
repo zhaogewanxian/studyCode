bc0a38a9f7640cc7b800c5c2c545ed98d70b7d25
zhaogewanxian <449244549@qq.com>
2019-04-09 11:08:50 +0800

    springboot-tomcat

--- spring-boot/spring-boot-tomcat/src/main/java/com/wanxian/tomcat/TomcatApplication.java
+++ spring-boot/spring-boot-tomcat/src/main/java/com/wanxian/tomcat/TomcatApplication.java
@@ -1,5 +1,6 @@
 package com.wanxian.tomcat;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11Nio2Protocol;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 
@@ -23,10 +25,16 @@ public class TomcatApplication {
             public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
                 if (configurableEmbeddedServletContainer instanceof TomcatEmbeddedServletContainerFactory) {
                     TomcatEmbeddedServletContainerFactory factory = TomcatEmbeddedServletContainerFactory.class.cast(configurableEmbeddedServletContainer);
+                    factory.addContextCustomizers(new TomcatContextCustomizer() {
+                        @Override
+                        public void customize(Context context) {
+                            //context.setPath("spring-boot");
+                        }
+                    });
                     factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
                         @Override
                         public void customize(Connector connector) {
-                            connector.setPort(8082);
+                            connector.setPort(8082); //设置端口
                             connector.setProtocol(Http11Nio2Protocol.class.getName());
                         }
                     });
