4dc0c081cbc753cd3837597884324ad78eb39948
zhaogewanxian <449244549@qq.com>
2019-04-14 23:36:02 +0800

    spring boot redis

--- spring-boot/spring-boot-cache/src/main/java/com/wanxian/repository/UserRepositoryImpl.java
+++ spring-boot/spring-boot-cache/src/main/java/com/wanxian/repository/UserRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.wanxian.repository;
 
 import com.wanxian.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.interceptor.CacheInterceptor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -11,20 +13,23 @@ import java.util.Map;
 public class UserRepositoryImpl implements UserRepository {
     private final Map<String, User> userRepositoty = new HashMap<>();
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public boolean save(User user) {
-        return userRepositoty.putIfAbsent(user.getId().toString(), user) == null;
+        redisTemplate.opsForValue().set(user.getId(), user);
+        return true;
+//        return userRepositoty.putIfAbsent(user.getId().toString(), user) == null;
     }
 
-
     /**
-
-     *
      * @param id
      * @return
      */
     @Override
     public User getUser(String id) {
-        return userRepositoty.get(id);
+        return (User) redisTemplate.opsForValue().get(id);
+//        return userRepositoty.get(id);
     }
 }
