211274322d7649b3992052632e7aa494c6769883
zhaogewanxian <449244549@qq.com>
2019-04-20 23:56:03 +0800

    sc  client server

--- spring-cloud/spring-cloud-project/spring-cloud-client-application/src/main/java/com/wanxian/springcloud/SpringCloudClientApplication.java
+++ spring-cloud/spring-cloud-project/spring-cloud-client-application/src/main/java/com/wanxian/springcloud/SpringCloudClientApplication.java
@@ -3,11 +3,8 @@ package com.wanxian.springcloud;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 /**
  *
