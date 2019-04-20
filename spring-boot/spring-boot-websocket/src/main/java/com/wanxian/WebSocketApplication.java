6509f776cf558071322370b2a0b6f8cfcca62c82
zhaogewanxian <449244549@qq.com>
2019-04-20 12:49:44 +0800

    spring boot websocket

--- spring-boot/spring-boot-websocket/src/main/java/com/wanxian/WebSocketApplication.java
+++ spring-boot/spring-boot-websocket/src/main/java/com/wanxian/WebSocketApplication.java
@@ -1,11 +1,14 @@
 package com.wanxian;
 
+import com.wanxian.websocket.ChatRoomServerEndpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
+@EnableWebSocket
 public class WebSocketApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebSocketApplication.class, args);
@@ -15,4 +18,11 @@ public class WebSocketApplication {
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
+
+    @Bean
+    public ChatRoomServerEndpoint chatRoomServerEndpoint() {
+        return new ChatRoomServerEndpoint();
+    }
+
+
 }
--- spring-boot/spring-boot-websocket/src/main/java/com/wanxian/websocket/ChatRoomServerEndpoint.java
+++ spring-boot/spring-boot-websocket/src/main/java/com/wanxian/websocket/ChatRoomServerEndpoint.java
@@ -4,25 +4,40 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 聊天室服务端
  */
 @ServerEndpoint("/chat-room/{username}")
 public class ChatRoomServerEndpoint {
+    private static Map<String, Session> livingSessions = new ConcurrentHashMap<String, Session>();
+
     @OnOpen
     public void onOpen(@PathParam("username") String username, Session session) {
-        sendText(session, "欢迎用户" + username + "来到聊天室");
+        String sessionId = session.getId();
+        livingSessions.put(sessionId, session);
+        //sendText(session, "欢迎用户" + username + "来到聊天室");
+        sendTextAll("欢迎用户" + username + "来到聊天室");
+
     }
 
     @OnMessage
     public void onMessage(@PathParam("username") String username, Session session, String message) {
-        sendText(session, "用户" + username + "：" + message);
+        sendTextAll("用户" + username + "：" + message);
+    }
+
+    private void sendTextAll(String message) {
+        livingSessions.forEach((sessionId, session) -> {
+            sendText(session, message);
+        });
     }
 
     @OnClose
     public void onClose(@PathParam("username") String username, Session session) {
-
+        livingSessions.remove(session.getId());
+        sendTextAll("用户" + username + "已经离开了聊天室");
     }
 
     public void sendText(Session session, String message) {
