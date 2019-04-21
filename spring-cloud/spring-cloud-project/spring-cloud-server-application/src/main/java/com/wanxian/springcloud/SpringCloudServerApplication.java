346cb96d3f851c8ef5e69fd710a4f6cb18348fc9
zhaogewanxian <449244549@qq.com>
2019-04-21 13:54:48 +0800

    sc  client server

--- spring-cloud/spring-cloud-project/spring-cloud-server-application/src/main/java/com/wanxian/springcloud/SpringCloudServerApplication.java
+++ spring-cloud/spring-cloud-project/spring-cloud-server-application/src/main/java/com/wanxian/springcloud/SpringCloudServerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class SpringCloudServerApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringCloudServerApplication.class).web(WebApplicationType.SERVLET).run(args);
---
+++ spring-cloud/spring-cloud-project/spring-cloud-server-application/src/main/java/com/wanxian/springcloud/web/mvc/CircuitBreakerControllerAdvic.java
@@ -0,0 +1,21 @@
+package com.wanxian.springcloud.web.mvc;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.TimeoutException;
+
+@RestControllerAdvice
+public class CircuitBreakerControllerAdvic {
+
+    @ExceptionHandler
+    public void onTimeoutException(TimeoutException exception, Writer writer) throws IOException {
+        writer.write(errorContent(""));
+    }
+
+    public String errorContent(String message) {
+        return "fallback";
+    }
+}
