4552823d62fab6ef562b5a5241f3d787488dd7b7
zhaogewanxian <449244549@qq.com>
2019-05-06 23:22:45 +0800

    spring cloud stream rabbitmq

--- spring-cloud-study/spring-cloud-project/spring-cloud-stream-rabbitmq/src/main/java/com/wanxian/springcloud/controller/ProducerController.java
+++ spring-cloud-study/spring-cloud-project/spring-cloud-stream-rabbitmq/src/main/java/com/wanxian/springcloud/controller/ProducerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * kafka生产者控制器
+ * 生产者控制器
  */
 @RestController
 public class ProducerController {
